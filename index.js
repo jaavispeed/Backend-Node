4bf8fb44a494b6f9a9b21aa75d4a2ecb608ea809
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-01-29 18:14:47 -0300

    Servidor andando

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+import 'dotenv/config';
 import express from 'express';
 
 const app = express();
 
 app.get('/', (req, res) => {
-    req.send('Hello World');
+    res.send('Hello World');
 });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
